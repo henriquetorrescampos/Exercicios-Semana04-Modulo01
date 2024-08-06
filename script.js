67ab0a0efe3b0b995eae2b2ccfa28e79f3180d47
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-05 21:51:31 -0300

    add new function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Script funcionando");
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function gerarResultado(event) {
+  event.preventDefault();
+
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  let resultado = sum(num1, num2);
+
+  alert(resultado);
+}
